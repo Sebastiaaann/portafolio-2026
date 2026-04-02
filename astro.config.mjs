294f3fd1e362e4deda190a89a4dcb2e5034f4cd5
Sebastiaaann <sebastian.almonacid06@inacapmail.cl>
2026-04-02 17:43:46 -0300

    chore: prepare vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://salmonacid.dev',
   output: 'static',
   integrations: [react()],
   vite: {
